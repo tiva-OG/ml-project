10d16dfcf808ab075a5847e91505b6fa9b41689c
Salvation Tiva <tivasalvation@gmail.com>
2023-12-04 15:00:52 +0100

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,12 +50,55 @@ class ModelTrainer:
                 "XGBoost Regressor": XGBRegressor(),
             }
 
+            params = {
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    # 'loss':['linear','square','exponential'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoost Regressor": {
+                    "depth": [6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
+                "DecisionTree Regressor": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "GradientBoosting Regressor": {
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "K-Neighbors Regressor": {},
+                "Linear Regressor": {},
+                "RandomForest Regressor": {
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
+                },
+                "XGBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
+
             models_report: dict = evaluate_models(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
                 models=models,
+                params=params,
             )
 
             # get best model and score
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import dill
+import pickle
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from src.logger import logging
 
 
 def save_object(file_path, obj):
@@ -18,14 +21,28 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
 
             # training
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             # prediction
@@ -38,6 +55,10 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             # report[list(models.keys())[i]] = {"train": train_score, "test": test_score}
             report[list(models.keys())[i]] = test_score
+
+            logging.info(
+                f"Training done on {list(models.keys())[i]}. train score: {train_score}; test score: {test_score}"
+            )
 
         return report
 
